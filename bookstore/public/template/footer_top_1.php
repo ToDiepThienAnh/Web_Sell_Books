65067dd5443a6811c36a3dc6d8ad7e97551c41a4
ToDiepThienAnh <51430603+ToDiepThienAnh@users.noreply.github.com>
2020-07-10 23:03:34 +0700

    PHP MVC

---
+++ bookstore/public/template/footer_top_1.php
@@ -0,0 +1,41 @@
+<section class="container-fluid footer-top1">
+    <section class="container">
+      <section class="row-fluid">
+        <figure class="span3">
+          <h4>Đăng ký nhận tin</h4>
+          <p>Đăng ký nhận thông tin sách mới, sách bán chạy và khuyến mãi từ VuonTriThuc</p>
+          <input name="" type="text" class="field-bg" value="Nhập địa chỉ email của bạn"/>
+          <input name="" type="submit" value="Đăng ký" class="sub-btn" />
+        </figure>
+        <figure class="span3">
+          <h4>Về Công Ty</h4>
+          <ul class="tweets-list">
+            <li><a href="#">Giới thiệu công ty</a></li>
+            <li><a href="#">Tuyển dụng</a></li>
+            <li><a href="#">Góc báo chí</a></li>
+            <li><a href="#">Chương trình đại lý</a></li>
+            <li><a href="#">Chính sách bảo mật</a></li>
+            <li><a href="#">Điều khoản sử dụng</a></li>
+            <li><a href="#"></a></li>
+          </ul>
+        </figure>
+        <figure class="span3">
+          <h4>Trợ giúp</h4>
+          <ul class="tweets-list">
+              <li><a href="#">Quy định sử dụng</a></li>
+              <li><a href="#">Hướng dẫn mua hàng</a></li>
+                <li><a href="#">Phương thức thanh toán</a></li>
+                <li><a href="#">Phương thức vận chuyển</a></li>
+                <li><a href="#">Chính sách đổi trả</a></li>
+                <li><a href="#">Các câu hỏi thường gặp</a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </figure>
+        <figure class="span3">
+          <h4>Địa chỉ</h4>
+            <p>khu phố 6, Thủ Đức, Hồ Chí Minh</p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.2324288146733!2d106.80161941476486!3d10.86991839225812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527587e9ad5bf%3A0xafa66f9c8be3c91!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2jhu4cgVGjDtG5nIHRpbiDEkEhRRyBUUC5IQ00!5e0!3m2!1svi!2s!4v1594376857920!5m2!1svi!2s" width="300" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" style="border:0" allowfullscreen></iframe>
+        </figure>
+      </section>
+    </section>
+  </section>
